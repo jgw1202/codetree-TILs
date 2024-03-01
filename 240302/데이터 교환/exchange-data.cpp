c76da8c1b2a381f9aeb16292df40eb1e4b1a884c
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 02:18:38 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/데이터 교환/exchange-data.cpp
+++ 240302/데이터 교환/exchange-data.cpp
@@ -4,9 +4,12 @@ int main() {
     int a = 5;
     int b = 6;
     int c = 7;
-    b = a;
-    c = b;
-    a = c;
+    int tmpa = a;
+    int tmpb = b;
+    int tmpc = c;
+    b = tmpa;
+    c = tmpb;
+    a = tmpc;
     cout << a << "\n";
     cout << b << "\n";
     cout << c;
