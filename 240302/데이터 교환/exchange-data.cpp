072ffcc5378ed8dcc5cab0192449eb6fe8fd03bc
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 02:15:28 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240302/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    b = a;
+    c = b;
+    a = c;
+    cout << a << endl;
+    cout << b << endll
+    cout << c;
+    return 0;
+}
