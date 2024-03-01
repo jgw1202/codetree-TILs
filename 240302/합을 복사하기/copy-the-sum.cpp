02ddf9a5e3da065b5bb5203c547c5c71733e4a40
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 02:20:02 +0900

    Title: 합을 복사하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/합을 복사하기/copy-the-sum.cpp
+++ 240302/합을 복사하기/copy-the-sum.cpp
@@ -4,9 +4,10 @@ int main() {
     int a = 1;
     int b = 2;
     int c = 3;
-    a = a+b+c;
-    b = a+b+c;
-    c = a+b+c;
+    int tmp = a+b+c;
+    a = tmp;
+    b = tmp;
+    c = tmp;
     cout << a << " " << b << " " << c;
     return 0;
 }
