d21be41a5a74bda718103762c62bda862054a2a1
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 21:15:53 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/4번의 크기 비교/4-time-comparison.cpp
+++ 240302/4번의 크기 비교/4-time-comparison.cpp
@@ -7,12 +7,12 @@ int main() {
    cin >> a;
    cin >> b >> c >> d >> e;
    if(a>b) cout << 1 << "\n";
-   else cout << 0 << "\n"'
+   else cout << 0 << "\n";
    if(a>c) cout << 1 << "\n";
-   else cout << 0 << "\n"'
+   else cout << 0 << "\n";
    if(a>d) cout << 1 << "\n";
-   else cout << 0 << "\n"'
+   else cout << 0 << "\n";
    if(a>e) cout << 1 << "\n";
-   else cout << 0 << "\n"'
+   else cout << 0 << "\n";
     return 0;
 }
