85ddb863d4a222ec87e6494b099b13e9698b9100
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 23:25:34 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240302/중앙값 구하기/find-the-median.cpp
+++ 240302/중앙값 구하기/find-the-median.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
 #include <algorithm>
 using namespace std;
+
 int main() {
-    int a,b,c;
-    cin >> a >> b >> c;
-    sort(a,b,c);
-    cout << b;
-    return 0;
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	int arr[3] = { a,b,c };
+	sort(arr, arr + 3);
+	cout << arr[1];
+	return 0;
 }
