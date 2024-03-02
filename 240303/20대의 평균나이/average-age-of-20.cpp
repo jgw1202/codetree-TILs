3a9bb02b9bb3c7d1b299ef4bbe7d445a59f0f2d3
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-03 00:15:28 +0900

    Title: 20대의 평균나이, Time: 1060ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240303/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int sum = 0;
+	double cnt = 0.0;
+	while (true) {
+		int n;
+		cin >> n;
+		if (n < 20 && n > 29) {
+			break;
+		}
+		sum += n;
+		cnt++;
+	}
+	cout << sum / cnt;
+	return 0;
+}
