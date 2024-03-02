27629dc6e9ffab7a699e274340267b22342f14aa
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-02 23:17:22 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240302/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    char b;
+    int c;
+    char d;
+    if(a>=19&&b=='M' || c>=19&&d=='M') cout << 1;
+    else cout << 0;
+    return 0;
+}
