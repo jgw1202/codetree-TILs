ffe8465cf288efbde5c1147d81f7bd0a8ddb0556
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-03 00:23:07 +0900

    Title: 조건에 따른 연산, Time: 1093ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240303/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	int cnt = 0;
+	if (n % 2 == 0) {
+		while (true) {
+			cnt++;
+			if (n >= 1000) break;
+			n = n * 3 + 1;
+		}
+	}
+	else {
+		while (true) {
+			cnt++;
+			if (n >= 1000) break;
+			n * 2 + 2;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
