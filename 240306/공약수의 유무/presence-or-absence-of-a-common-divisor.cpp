1693fb4d772da79c6e492247ff71038149c30c46
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-06 19:36:23 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240306/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
+#include <vector> // vector 헤더 파일을 포함해야 합니다.
 
 using namespace std;
 
+// 최대공약수를 구하는 함수 (유클리드 호제법)
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// 주어진 수의 약수를 구하는 함수
+vector<int> get_divisors(int num) {
+    vector<int> divisors;
+    for (int i = 1; i <= num; ++i) {
+        if (num % i == 0) {
+            divisors.push_back(i);
+        }
+    }
+    return divisors;
 }
 
 int main() {
     int a, b;
     cin >> a >> b;
 
-    bool common_divisor_exists = false;
+    int gcd_result = gcd(1920, 2880);
 
-    int common_divisor = gcd(1920, 2880);
+    vector<int> common_divisors = get_divisors(gcd_result);
 
-
-    for (int i = a; i <= b; ++i) {
-        if (i % common_divisor > 1 && common_divisor != 1) { 
-            common_divisor_exists = true;
-            break;
+    bool satisfied = false;
+    for (int i = a; i <= b; i++) {
+        for (int j = 0; j < common_divisors.size(); j++) {
+            if (i == common_divisors[j]) {
+                satisfied = true;
+                break;
+            }
         }
+        if (satisfied) // 만족하는 경우에는 반복문을 더 이상 수행할 필요가 없으므로 바로 종료합니다.
+            break;
     }
 
-
-    if (common_divisor_exists) {
+    if (satisfied)
         cout << 1 << endl;
-    } else {
+    else
         cout << 0 << endl;
-    }
 
     return 0;
 }
