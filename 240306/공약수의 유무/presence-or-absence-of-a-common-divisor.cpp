f3a75c86106233b242ee4bcf1c46166aff0357ed
정건우 <80203261+jgw1202@users.noreply.github.com>
2024-03-06 19:07:42 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240306/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -7,21 +7,19 @@ int gcd(int a, int b) {
         return a;
     return gcd(b, a % b);
 }
-
-bool hasCommonDivisor(int start, int end) {
-    for (int i = start; i <= end; ++i) {
-        if (gcd(i, 1920) != 1 && gcd(i, 2880) != 1) {
-            return true;
-        }
-    }
-    return false;
-}
-
 int main() {
     int a, b;
     cin >> a >> b;
 
-    if (hasCommonDivisor(a, b)) {
+    bool found = false;
+    for (int i = a; i <= b; ++i) {
+        if (gcd(i, 1920) != 1 && gcd(i, 2880) != 1) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
         cout << 1 << endl;
     } else {
         cout << 0 << endl;
